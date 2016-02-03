d9abffb34541df006ce301691155e77504a6fef4
dev3-825 <dev3@eight25media.com>
2016-02-03 22:31:33 +0530

    retry

--- public/app/components/elements/InputField.js
+++ public/app/components/elements/InputField.js
@@ -1,13 +1,76 @@
 //InputField
 import React from 'react';
+import {Alert} from '../../config/Alert';
+
+let errorStyles = {
+	color         : "#ed0909",
+	fontSize      : "0.8em",
+	textTransform : "capitalize",
+	margin        : '10px 0 0',
+	display       : "inline-block"
+}
+
 export default class InputField extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {valueTxt: "", validate: ""};
+	}
+
+	handleChange(e) {
+		this.props.textChange(e.target.value);
+	}
+
+	handleBlur(e){
+		let value = e.target.value;
+
+		if(value.length > 0){
+
+			if(this.props.type == "text"){
+				
+			}
+
+			if(this.props.type == "password"){
+				if(value.length <=6){
+					this.setState({validate: Alert.PASSWORD_LENGTH_ERROR});
+				}else{
+					this.setState({validate: ""});
+				}
+			}
+
+			if(this.props.type == "email"){
+				var regx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				
+				if(!regx.test(value)){
+					this.setState({validate: Alert.INVALID_EMAIL});
+				}else{
+					this.setState({validate: ""});
+				}
+			}
+
+		}else{
+			this.setState({validate: ""});
+		}
+
+	}
+
 	render() {
-		let size = "col-xs-" + this.props.size;
+		let size = "input-field col-xs-" + this.props.size;
 
 		return (
 			<div className={size}>
 				<p>{this.props.label}</p>
-				<input type={this.props.type} placeholder={this.props.placeholder} className={this.props.classes} />
+
+				<input type={this.props.type} 
+					name={this.props.name} 
+					value={this.props.valueTxt} 
+					placeholder={this.props.placeholder} 
+					className={this.props.classes} 
+					onChange={this.handleChange.bind(this)}
+					onBlur={this.handleBlur.bind(this)} />
+
+				{this.state.validate ? <span className="invalid-msg" style={errorStyles}>{this.state.validate}</span> : null}
+
 			</div>
 		);
 	}
--- public/app/pages/Signup/Signup.js
+++ public/app/pages/Signup/Signup.js
@@ -1,7 +1,41 @@
 import React from 'react'
 import InputField from '../../components/elements/InputField'
+import Button from '../../components/elements/Button'
 
 class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {formData: []};
+
+        this.fnameChangeHandler = this.fnameChangeHandler.bind(this)
+        this.lnameChangeHandler = this.lnameChangeHandler.bind(this)
+    }
+
+    validateForm(e){
+        e.preventDefault();
+        console.log( this.state.formData);
+    }
+
+    fnameChangeHandler(data){
+        this.setState({
+            formData:
+                {'fname':data}
+        });
+        console.log(data)
+    }
+
+    lnameChangeHandler(data){
+        this.setState({
+            formData:
+                {'lname':data}
+        });
+        console.log(data)
+    }
+
+    handleChange(e){
+        
+        console.log(  this.state.formData);
+    }
 
 	render(){
 
@@ -12,28 +46,23 @@ class Signup extends React.Component {
                         <div className="row row-clr pgs-middle-sign-wrapper-inner-cover">
                         	<h2>Let’s create your account</h2>
                             <div className="row row-clr pgs-middle-sign-wrapper-inner-form">
-                                <form method="get" action="/choose-secretary">
+                                <form method="get" onSubmit={this.validateForm.bind(this)} >
                                     <div className="row">
 
-                                        <InputField type="text" size="6" label="First Name" placeholder="Soham" classes="pgs-sign-inputs" />
+                                        <InputField type="text" name="fName" size="6" label="First Name" placeholder="Soham" classes="pgs-sign-inputs" textChange={this.fnameChangeHandler} />
 
-                                        <InputField type="text" size="6" label="Last Name" placeholder="Khaitan" classes="pgs-sign-inputs" />
-
+                                        <InputField type="text" name="lName" size="6" label="Last Name" placeholder="Khaitan" classes="pgs-sign-inputs" textChange={this.lnameChangeHandler} />
                                     </div>
                                     <div className="row">
-                                        <InputField type="email" size="12" label="Your email address" placeholder="sohamkhaitan@gmail.com" classes="pgs-sign-inputs" />
+                                        <InputField type="email" name="email" size="12" label="Your email address" placeholder="sohamkhaitan@gmail.com" classes="pgs-sign-inputs" />
                                     </div>
                                     <div className="row">
-                                        <InputField type="password" size="6" label="Password" placeholder="••••••••••" classes="pgs-sign-inputs" />
-                                        <InputField type="password" size="6" label="Confirm Password" placeholder="••••••••••" classes="pgs-sign-inputs" />
+                                        <InputField type="password" name="password" size="6" label="Password" placeholder="••••••••••" classes="pgs-sign-inputs" />
+                                        <InputField type="password" name="confPassword" size="6" label="Confirm Password" placeholder="••••••••••" classes="pgs-sign-inputs" />
                                     </div>
                                     <div className="row">
-                                        <div className="col-xs-6">
-                                            <input type="reset" className="pgs-sign-submit-cancel" value="cancel"/>
-                                        </div>
-                                        <div className="col-xs-6">
-                                            <input type="submit" className="pgs-sign-submit" value="next"/>
-                                        </div>
+                                        <Button type="reset" size="6" classes="pgs-sign-submit-cancel" value="cancel" />
+                                        <Button type="submit" size="6" classes="pgs-sign-submit" value="next" />
                                     </div>
                                 </form>    
                             </div>
