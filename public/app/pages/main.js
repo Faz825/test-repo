ae2c7819779ad32fe44847e80bef5cc76e3bc28f
dev4-825 <dev4@eight25media.com>
2016-02-05 14:31:11 +0000

    Rename Main js

--- public/app/pages/main.js
+++ public/app/pages/main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/header/Header'
-import Signup from '../pages/Signup/Signup';
+import Signup from '../pages/signup/Signup';
 class Main extends React.Component {
   render() {
     console.log(logged_user);
