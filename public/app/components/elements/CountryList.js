f1ea26a0ac82b9d5ac76c749e46caeb803a37796
dev3-825 <dev3@eight25media.com>
2016-02-09 20:05:26 +0530

    Completed the About you form component

---
+++ public/app/components/elements/CountryList.js
@@ -0,0 +1,38 @@
+//CountryList
+import React from 'react'
+import {Countries} from '../../service/Countries'
+
+export default class CountryList extends React.Component{ 
+	constructor(props) {
+        super(props);
+        this.selectChange = this.selectChange.bind(this);
+    }
+
+    selectChange(e){
+
+    	if(this.props.required){ 
+			if(e.target.value.length != 0 ){
+				status = "valid";
+			}else{
+				status = "invalid";
+			}
+		}else{
+			status = "";
+		}
+
+		this.props.optChange("country",e.target.value,status);
+    }
+
+	render(){
+		return(
+			<div className="col-xs-5">
+	            <p>Country</p>
+	            <select name="country" className="pgs-sign-select" onChange={this.selectChange.bind(this)}>
+	            	{Countries.map(function(country, i){
+						return <option value={country.name} key={i}>{country.name}</option>;
+	            	})}
+	            </select>
+            </div>
+		); 
+	}
+}
--- public/app/pages/signup/AboutYou.js
+++ public/app/pages/signup/AboutYou.js
@@ -1,4 +1,3 @@
-//AboutYou
 import React from 'react'
 import InputField from '../../components/elements/InputField'
 import SelectDateDropdown from '../../components/elements/SelectDateDropdown'
@@ -6,11 +5,21 @@ import CountryList from '../../components/elements/CountryList'
 import Button from '../../components/elements/Button'
 import {Alert} from '../../config/Alert'
 
+let errorStyles = {
+    color         : "#ed0909",
+    fontSize      : "0.8em",
+    textTransform : "capitalize",
+    margin        : '0 0 15px',
+    display       : "inline-block"
+}
+
 export default class AboutYou extends React.Component{
 	constructor(props) {
         super(props);
         this.state= {
-            formData:{}//,
+            formData:{},
+            errorData:{},
+            validateAlert: ""
             //signupURL:'/doSignup'
         };
         this.collectData = this.collectData.bind(this);
@@ -18,19 +27,43 @@ export default class AboutYou extends React.Component{
         
     }
 
-    elementChangeHandler(key,data){
+    allInvalid(elements) {
+        for (var i in elements) {
+            if (elements[i]["status"] == "invalid") return false;
+        }
+        return true;
+    }
+
+    elementChangeHandler(key,data,status){
         
         let _formData = this.state.formData;
+        let _errorData = this.state.errorData;
+
         _formData[key] = data;
         this.setState({formData:_formData});
 
+        if(status != ""){
+            _errorData[key] = {"status": status};
+            this.setState({errorData:_errorData});
+        }
+    
     }
 
     collectData(e){
-    	e.preventDefault();
+        e.preventDefault();
 
-    	console.log("hi");
-    	console.log(this.state.formData);
+        if(Object.keys(this.state.errorData).length != 2){
+            this.setState({validateAlert: Alert.FILL_EMPTY_REQUIRED_FIELDS});
+        }else{
+            this.setState({validateAlert: Alert.FILL_EMPTY_REQUIRED_FIELDS});
+
+            if(this.allInvalid(this.state.errorData)){
+                this.setState({validateAlert: ""});
+
+                console.log(this.state.formData);
+            }
+        }
+
     }
 
 	render(){
@@ -61,13 +94,15 @@ export default class AboutYou extends React.Component{
                                     	<h6>First, Let me know a little more about you...</h6>
                                         <form method="post" onSubmit={this.collectData.bind(this)}>
 	                                        <div className="row pgs-middle-about-inputs">
-	                                        	<SelectDateDropdown title="Date of Birth" dateFormat="dd/mm/yyyy" optChange={this.elementChangeHandler} />
+	                                        	<SelectDateDropdown title="Date of Birth" dateFormat="dd-mm-yyyy" optChange={this.elementChangeHandler} required="true"/>
 
-	                                            <CountryList optChange={this.elementChangeHandler} />
+	                                            <CountryList optChange={this.elementChangeHandler} required="true"/>
 
-	                                            <InputField type="text" name="zip" size="2" label="Zip Code" placeholder="98252" classes="pgs-sign-inputs" textChange={this.elementChangeHandler}/>
+	                                            <InputField type="text" name="zip" size="2" label="Zip Code" placeholder="98252" classes="pgs-sign-inputs" textChange={this.elementChangeHandler} />
 	                                        </div>
-	                                        
+
+	                                        {this.state.validateAlert ? <p className="form-validation-alert" style={errorStyles} >{this.state.validateAlert}</p> : null}
+
 	                                        <div className="row">
 		                                        <Button type="button" size="6" classes="pgs-sign-submit-cancel" value="cancel" />
 		                                        <Button type="submit" size="6" classes="pgs-sign-submit" value="next" />
