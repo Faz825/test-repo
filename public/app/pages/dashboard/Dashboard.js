7bcc76889eea8209a3fefaeb1a1ea3031a13a9da
dev3-825 <dev3@eight25media.com>
2016-02-26 12:24:18 +0530

    Added dashboard and fixed styles.

--- public/app/pages/dashboard/Dashboard.js
+++ public/app/pages/dashboard/Dashboard.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import Session  from '../../middleware/Session';
 
 class Dashboard extends React.Component {
 
 	render(){
+		let session = Session.getSession('prg_lg');
 
 		return (
-			<div>
-				DashBoard
-			</div>
+			<div className="pg-page" id="pg-dashboard-page">
+                <div className="row row-clr" id="pg-dashboard-banner-area">
+                    <img src="images/logo-large-middle.png" className="img-responsive center-block" id="pg-dashboard-banner-area-logo-img" />
+                    <h2 id="pg-dashboard-banner-area-title-wish">Good Morning, {session.first_name + " " + session.last_name}</h2>
+                    <h1 id="pg-dashboard-banner-area-title-what-to-do">What shall we do today?</h1>
+                </div>
+            </div>
 		)
 	}
 
