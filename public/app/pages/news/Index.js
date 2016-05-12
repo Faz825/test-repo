6343c6b6294b5b3bc45b55a90558d749b431a62d
dev4-825 <dev4@eight25media.com>
2016-05-12 05:41:18 +0000

    merge bug fixing

--- public/app/pages/news/Index.js
+++ public/app/pages/news/Index.js
@@ -79,13 +79,13 @@ export default class Index extends React.Component{
         }).done( function (data, text) {
             if(data.status.code == 200){
                 this.setState({news_articles:data.news});
-                //this.setState({display_news_articles:data.news});
-                _this.getRandomNewsArticles();
-                if(data.news.length > 10){
-                    this.refreshInterval = setInterval(function(){_this.getRandomNewsArticles()}, 60000);
-                }else{
-                    this.setState({display_news_articles:data.news});
-                }
+                this.setState({display_news_articles:data.news});
+                //_this.getRandomNewsArticles();
+                //if(data.news.length > 10){
+                //    this.refreshInterval = setInterval(function(){_this.getRandomNewsArticles()}, 60000);
+                //}else{
+                //    this.setState({display_news_articles:data.news});
+                //}
             }
         }.bind(this));
     }
