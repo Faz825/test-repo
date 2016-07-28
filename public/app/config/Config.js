e371dc5624f09072a3a6e8f21306c2024fd701a8
dev4-825 <dev4@eight25media.com>
2016-07-28 08:32:41 +0000

    deploy

--- public/app/config/Config.js
+++ public/app/config/Config.js
@@ -1,6 +1,6 @@
 const Config = {
-    "PROGLOBE_APP" : "//proglobe.loc/",
-    "PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.loc/"
+    "PROGLOBE_APP" : "https://dev.proglobe.us/",
+    "PROGLOBE_NOTIFICATION_APP" : "https://dev-notification.proglobe.us/"
 }
 
 
