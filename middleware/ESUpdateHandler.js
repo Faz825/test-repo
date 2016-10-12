1d2769d7decb67d71843fe2ff479bcfbfab856d3
dev2-825 <dev2@eight25media.com>
2016-10-12 19:26:12 +0530

    ES Update handler

--- middleware/ESUpdateHandler.js
+++ middleware/ESUpdateHandler.js
@@ -46,18 +46,22 @@ var ESUpdateHandler = {
                                     _async.eachSeries(esResult, function(esUser,callBack){
                                         if(typeof esUser.connected_at != 'undefined'){
                                             if(typeof friends[esUser.user_id] != 'undefined') {
-                                                esUser['connected_at'] = friends[esUser.user_id].updated_at;
+                                                if(typeof friends[esUser.user_id].updated_at != 'undefined') {
+                                                    esUser['connected_at'] = friends[esUser.user_id].updated_at;
 
-                                                var payLoad = {
-                                                    index: _cache_key,
-                                                    id: esUser.user_id,
-                                                    type: 'connections',
-                                                    data: esUser
-                                                }
+                                                    var payLoad = {
+                                                        index: _cache_key,
+                                                        id: esUser.user_id,
+                                                        type: 'connections',
+                                                        data: esUser
+                                                    }
 
-                                                ES.update(payLoad, function (resultSet) {
+                                                    ES.update(payLoad, function (resultSet) {
+                                                        callBack(null);
+                                                    });
+                                                }else{
                                                     callBack(null);
-                                                });
+                                                }
                                             }else{
                                                 callBack(null);
                                             }
