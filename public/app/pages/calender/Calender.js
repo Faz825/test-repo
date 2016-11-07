8701de1bc10fb67e23850cbaf9365cf1c87907d4
dev5-825 <dev5@eight25media.com>
2016-11-07 11:34:55 +0530

    view change - 02

--- public/app/pages/calender/Calender.js
+++ public/app/pages/calender/Calender.js
@@ -48,14 +48,14 @@ export default class Calender extends React.Component {
         var month = this.state.month;
         month.add(-1, "M");
         this.setState({ month: month });
-        this.getAllEventsForMonth();
+        //this.getAllEventsForMonth();
     }
 
     next() {
         var month = this.state.month;
         month.add(1, "M");
         this.setState({ month: month });
-        this.getAllEventsForMonth();
+        //this.getAllEventsForMonth();
     }
 
     select(day) {
