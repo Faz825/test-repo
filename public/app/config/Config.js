515464c3b7aa60f86aad66871ff276692f52a432
dev5-825 <dev5@eight25media.com>
2016-11-07 11:55:28 +0530

    notification url change-01

--- public/app/config/Config.js
+++ public/app/config/Config.js
@@ -1,9 +1,9 @@
 const Config = {
     //"PROGLOBE_APP" : "//proglobe.loc/",
-    "PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.loc/",
+    //"PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.loc/",
 
     "PROGLOBE_APP" : "//dev.proglobe.us/",
-    //"PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/"
+    "PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/"
     //"PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.local/"
 
 
