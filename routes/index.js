ee4633a277bfcdeb2fa896f7e230a35dc293f534
dev5-825 <dev5@eight25media.com>
2016-12-14 13:38:33 +0530

    webpack bundler -03

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-require('../model/UserModel');
-
 var UserControler = require('../controller/UserController');
 router.get('/auth/*', UserControler.doSignup);
 
-
-
-
-
-
-
-var UserControler = require('../controller/UserController');
 /* Load Default Routes  */
 router.get('/*', function(req, res, next) {
   res.render('index');
 });
-
-
-
-
 
 
 module.exports = router;
