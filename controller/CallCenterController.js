e374765bc770ab13388cccf7d444a96bffe8cc25
dev4-825 <dev4@eight25media.com>
2016-12-19 10:00:25 +0530

    removed un-necessary comments

--- controller/CallCenterController.js
+++ controller/CallCenterController.js
@@ -1,14 +1,6 @@
-/**
- * Created by gihan on 12/7/16.
- */
-
 'use strict';
 
-/**
- * The CallCenter Controller
- */
 var CallCenterController = {
-
     /**
      * add call center record
      * @param req
