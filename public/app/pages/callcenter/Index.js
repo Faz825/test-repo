8456a43ce20d24a5bb43c9d7a405e5d32f9f4dfb
dev3-825 <dev3@eight25media.com>
2016-12-28 11:11:51 +0530

    Callcenter Common stat config file added

--- public/app/pages/callcenter/Index.js
+++ public/app/pages/callcenter/Index.js
@@ -8,6 +8,8 @@ import {Modal, ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
 import Session from '../../middleware/Session';
 import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 import ContactList from "./ContactList";
+import RecentList from "./RecentList";
+import StatusList from "./StatusList";
 import User from "./User";
 import CallModel from "./CallModel";
 import CallHandler from './CallHandler';
@@ -476,6 +478,12 @@ export default class Index extends React.Component {
                                 :
                                 null
                         }
+                        {
+                            (mainCat == "status") ?
+                                <StatusList userList={this.state.userList} onUserCall={this.onUserCalling.bind(this)}/>
+                                :
+                                null
+                        }
                     </section>
                     {
                     	(this.state.minimizeBar)?
@@ -493,35 +501,6 @@ export default class Index extends React.Component {
                 {this.userCallPopup()}
                 {this.callPopup()}
             </section>
-        );
-    }
-}
-
-export class RecentList extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {}
-
-    }
-
-    onCalling(user,callType) {
-        this.props.onUserCall(user,callType);
-    }
-
-    render() {
-        let _this = this;
-        let recentList = this.props.userList.map(function (user, key) {
-            return (
-                <User users={user.users} type="recent" key={key} onCalling={_this.onCalling.bind(_this)}/>
-            )
-        })
-        return (
-            <div className="recent-list">
-                <div className="list-wrapper">
-                    {recentList}
-                </div>
-            </div>
         );
     }
 }
