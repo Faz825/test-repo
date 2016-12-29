77e69a2e048eb635fed48ad382ad0b96e3a70e20
dev4-825 <dev4@eight25media.com>
2016-12-29 16:30:09 +0530

    user onlinemode added to elasticsearch

--- public/app/config/Config.js
+++ public/app/config/Config.js
@@ -4,12 +4,12 @@ const Config = {
     // "PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.us/"
 
     /* Dev Server */
-    "PROGLOBE_APP" : "//dev.proglobe.us/",
-    "PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/"
+   // "PROGLOBE_APP" : "//dev.proglobe.us/",
+   // "PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/"
 
     /* Local Server  - change these as you wish */
-    // "PROGLOBE_APP" : "//dev.proglobe.us/",
-    // "PROGLOBE_NOTIFICATION_APP" : "//proglobe_notify.local/"
+     "PROGLOBE_APP" : "//dev.proglobe.us/",
+     "PROGLOBE_NOTIFICATION_APP" : "//proglobe_notify.local/"
 }
 
 export {Config};
