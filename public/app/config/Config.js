610bab87aafb5860ef629e3cdfc75f8a61b03bea
dev4-825 <dev4@eight25media.com>
2017-01-04 13:24:02 +0530

    develop call initiation

--- public/app/config/Config.js
+++ public/app/config/Config.js
@@ -4,13 +4,14 @@ const Config = {
     // "PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.us/"
 
     /* Dev Server */
-    // "PROGLOBE_APP" : "//dev.proglobe.us/",
-    // "PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/"
+    "PROGLOBE_APP": "//dev.proglobe.us/",
+    "PROGLOBE_NOTIFICATION_APP": "//dev-notification.proglobe.us/"
 
     /* Local Server  - change these as you wish */
-    "PROGLOBE_APP": "//dev.proglobe.us/",
-    "PROGLOBE_NOTIFICATION_APP": "//proglobe_notify.local/",
-    "BIT6_API_KEY": "22n7t-0zXi33P9Yap6"
+    /*"PROGLOBE_APP": "//dev.proglobe.us/",
+     "PROGLOBE_NOTIFICATION_APP": "//proglobe_notify.local/",
+     "BIT6_API_KEY": "22n7t-0zXi33P9Yap6",
+     "BIT6_IDENTITY_USER_SLUG":"usr:proglobe"*/
 }
 
 export {Config};
--- public/app/middleware/CallCenter.js
+++ public/app/middleware/CallCenter.js
@@ -4,12 +4,56 @@
 
 import Socket from './Socket';
 import Session from '../middleware/Session';
-import {Config} from '../config/Config'
+import {Config} from '../config/Config';
 
 class CallCenter {
 
     constructor() {
         this.socket = Socket.socket;
+
+        var bit6Opts = {'apikey': Config.BIT6_API_KEY};
+        this.b6 = new bit6.Client(bit6Opts);
+    }
+
+    initBit6(b6) {
+        var _this = this;
+
+        bit6Auth(false);
+
+        function bit6Auth(isNewUser) {
+            if (Session.getSession('prg_lg') != null) {
+                var oUser = Session.getSession('prg_lg');
+
+                if (b6.session.authenticated) {
+                    b6.session.displayName = oUser.first_name + " " + oUser.last_name;
+                    return true;
+                }
+
+                // Convert username to an identity URI
+
+                var ident = _this.getBit6Identity(oUser);
+                var pass = 'proglobe_' + oUser.id;
+
+                // Call either login or signup function
+                var fn = isNewUser ? 'signup' : 'login';
+                b6.session[fn]({'identity': ident, 'password': pass}, function (err) {
+                    if (err) {
+                        bit6Auth(true);
+                    }
+                    else {
+                        b6.session.displayName = oUser.first_name + " " + oUser.last_name;
+                        return true;
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * @param oUser - logged user object
+     * **/
+    getBit6Identity(oUser) {
+        return Config.BIT6_IDENTITY_USER_SLUG + oUser.user_name;
     }
 
     /**
