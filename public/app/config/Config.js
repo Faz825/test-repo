6a041c9989e1ea146bd05ecbc9ec58e77e3e6297
dev5-825 <dev5@eight25media.com>
2017-01-04 12:09:13 +0530

    config changes again-02

--- public/app/config/Config.js
+++ public/app/config/Config.js
@@ -1,16 +1,18 @@
 const Config = {
     /* Live Server  */
     // "PROGLOBE_APP" : "//proglobe.us/",
-    // "PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.us/"
+    // "PROGLOBE_NOTIFICATION_APP" : "//notification.proglobe.us/",
+    //"BIT6_API_KEY": "18j5x-sRBAbzhTW1ZD"
 
     /* Dev Server */
-    // "PROGLOBE_APP" : "//dev.proglobe.us/",
-    // "PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/"
+    "PROGLOBE_APP" : "//dev.proglobe.us/",
+    "PROGLOBE_NOTIFICATION_APP" : "//dev-notification.proglobe.us/",
+    "BIT6_API_KEY": "18j5x-sRBAbzhTW1ZD"
 
     /* Local Server  - change these as you wish */
-    "PROGLOBE_APP": "//dev.proglobe.us/",
-    "PROGLOBE_NOTIFICATION_APP": "//proglobe_notify.local/",
-    "BIT6_API_KEY": "22n7t-0zXi33P9Yap6"
+    // "PROGLOBE_APP": "//dev.proglobe.us/",
+    // "PROGLOBE_NOTIFICATION_APP": "//proglobe_notify.local/",
+    // "BIT6_API_KEY": "22n7t-0zXi33P9Yap6"
 }
 
 export {Config};
