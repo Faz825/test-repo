da0abf8e8ae39c6b7fbd477c486de898022e8ebd
dev4-825 <dev4@eight25media.com>
2017-03-31 10:05:39 +0530

    The timeSujestions list is modified

--- public/app/pages/calender/timeSuggestions.js
+++ public/app/pages/calender/timeSuggestions.js
@@ -4,8 +4,17 @@ import { fromJS } from 'immutable';
 let timeArr = [];
 for (var i = 0; i <= 23; i++) {
     for (var j = 0; j <= 59; j = j+30) {
+
+        var nameHr = i;
+        if(i>12) {
+            nameHr = i-12;
+        }else if (i=0) {
+            nameHr = 12;
+        }
+        var timeLable = (i>11) ? 'PM' : 'AM';
+
         let timeObj = {
-            name : String("0" + i).slice(-2)+'.'+String("0" + j).slice(-2),
+            name : String("0" + nameHr).slice(-2)+'.'+String("0" + j).slice(-2)+' '+timeLable,
             value : String("0" + i).slice(-2)+':'+String("0" + j).slice(-2),
             title : '',
             avatar : '/images/clock.png',
