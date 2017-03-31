5c5a006177a103f52dbeb8510ef41280857d3320
dev2-825 <dev2@eight25media.com>
2017-03-31 20:07:37 +0530

    Cover picture  -status

--- public/app/components/timeline/ListPostsElement.js
+++ public/app/components/timeline/ListPostsElement.js
@@ -984,6 +984,11 @@ export const UpdatedProPic = ({loggedUser,post})=>{
             <span className="own-post-share">has updated profile picture.</span>
         )
     }
+    if(post.post_mode == "CP"){
+        return (
+            <span className="own-post-share">has updated cover picture.</span>
+        )
+    }
     return (<span />);
 }
 
