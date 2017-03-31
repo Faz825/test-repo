5e75f17551f92b770a2d2739848e05ac7f9ef224
dev4-825 <dev4@eight25media.com>
2017-03-31 10:59:50 +0530

    24 hrs time suggestions is done

--- public/app/pages/calender/timeSuggestions.js
+++ public/app/pages/calender/timeSuggestions.js
@@ -1,18 +1,17 @@
 import { fromJS } from 'immutable';
 
-
 let timeArr = [];
 for (var i = 0; i <= 23; i++) {
+    var nameHr = i;
+    if(i>12) {
+        nameHr = nameHr-12;
+    }
+    if (i===0) {
+        nameHr = 12;
+    }
+
+    var timeLable = (i>11) ? 'PM' : 'AM';
     for (var j = 0; j <= 59; j = j+30) {
-
-        var nameHr = i;
-        if(i>12) {
-            nameHr = i-12;
-        }else if (i=0) {
-            nameHr = 12;
-        }
-        var timeLable = (i>11) ? 'PM' : 'AM';
-
         let timeObj = {
             name : String("0" + nameHr).slice(-2)+'.'+String("0" + j).slice(-2)+' '+timeLable,
             value : String("0" + i).slice(-2)+':'+String("0" + j).slice(-2),
@@ -24,5 +23,4 @@ for (var i = 0; i <= 23; i++) {
 }
 
 const mentions = fromJS(timeArr);
-
 export default mentions;
